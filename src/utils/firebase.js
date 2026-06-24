31c7475419ca00a245845bb1943543d2383dab55
Rakib <nuraarfin928@gmail.com>
2026-06-24 07:04:45 +0600

    Add comment to firebase config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -14,3 +14,4 @@ const firebaseConfig = {
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+// firebase
